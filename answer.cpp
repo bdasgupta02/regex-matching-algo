0ada171d22a9aaaf588b1d454f9b2acc44dd53f7
Bikramjit Dasgupta <55979037+bdasgupta02@users.noreply.github.com>
2021-09-28 15:00:58 +0800

    add memoization

--- answer.cpp
+++ answer.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.length(), n = p.length();
-        return backtracking(s, m, p, n);
+        int slen = s.length();
+        int plen = p.length();
+        vector<vector<int>> mem(slen+1, vector<int>(plen+1, -1));
+        helper(s, slen, p, plen, mem);
+        return mem[slen][plen] == 1 ? true : false;
     }
-    
-    bool backtracking(string& s, int i, string& p, int j) {
-        if (i == 0 && j == 0) return true;
-        if (i != 0 && j == 0) return false;
-        if (i == 0 && j != 0) {
-            if (p[j-1] == '*') {
-                return backtracking(s, i, p, j-2);
-            }
-            return false;
+    int helper(string& s, int i, string& p, int j, vector<vector<int>>& mem) {
+        int isMatch = -1;
+        if(mem[i][j] != -1)
+            return mem[i][j];
+        if(i == 0 && j == 0) {
+            mem[i][j] = 1; 
+            return 1;
         }
-
-        if (s[i-1] == p[j-1] || p[j-1] == '.') {
-            return backtracking(s, i - 1, p, j - 1);
-        } else if (p[j-1] == '*') {
-            if (backtracking(s, i, p, j - 2)) return true;
-            if (p[j-2] == s[i-1] || p[j-2] == '.') {
-                return backtracking(s, i - 1, p, j);
-            }
-            return false;
+        if(j == 0 && i > 0) {
+            mem[i][j] = 0;
+            return 0;
         }
-        return false;
+        if(i == 0 && j > 0) {
+            if(p[j-1] == '*') {
+                isMatch = helper(s, i, p, j-2, mem);
+                mem[i][j] = isMatch;
+                return isMatch;
+            }
+            return 0;
+        }
+        if(s[i-1] == p[j-1] || p[j-1] == '.') {
+            isMatch = helper(s, i-1, p, j-1, mem);
+            mem[i][j] = isMatch; 
+            return isMatch;
+        }else if(p[j-1] == '*') {
+            if(helper(s, i, p, j-2, mem) == 1) {
+                mem[i][j] = 1;
+                return 1;
+            }
+            if(p[j-2] == s[i-1] || p[j-2] == '.') {
+                isMatch = helper(s, i-1, p, j, mem);
+                mem[i][j] = isMatch;
+                return isMatch;
+            }
+        }
+        return isMatch;
     }
 };
